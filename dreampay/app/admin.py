8b7b44185fa8c424fef82dabe8d66819ddf2687c
Anvarisy <anvarisy@gmail.com>
2022-03-17 00:11:09 +0700

    Update admin.py

--- dreampay/app/admin.py
+++ dreampay/app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from app.models import Admin,AdminToken,Merchant,MerchantToken,Client,ClientToken,Cashier,CashierToken,Log,TopupLog,WithdrawlLog,TransactionLog
 # Register your models here.
+admin.site.register([Admin,AdminToken,Merchant,MerchantToken,Client,ClientToken,Cashier,CashierToken,Log,TopupLog,WithdrawlLog,TransactionLog])
